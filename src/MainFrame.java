71ed190856d32a9334bcb843a204a73e32461917
novator63 <pachk.kir@gmail.com>
2025-04-10 21:07:55 +0400

    demo

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 public class MainFrame extends JFrame {
     public MainFrame() {
         // Настройка главного окна
-        setTitle("Суперапп");
+        setTitle("Суперапп"); // some comment
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
